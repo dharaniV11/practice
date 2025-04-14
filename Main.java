c6e086f4161a18aae74740c65d7260dd7f8d4cd9
dharaniV11 <126811335+dharaniV11@users.noreply.github.com>
2025-04-14 16:03:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Student {
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Student{" + "age=" + age + ", name= " + name + '}';
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Comparator<Student> comparator=new Comparator<Student>() {
+            public int compare(Student i, Student j) {
+                if(i.age > j.age)
+                    return 1;
+                else
+                    return -1;
+            }
+        };
+        List<Student> number = new ArrayList<Student>();
+        number.add(new Student(12,"Bharath"));
+        number.add(new Student(15,"sarath"));
+        number.add(new Student(17,"mani"));
+        number.add(new Student(19,"ragul"));
+        number.add(new Student(21,"karthi"));
+        Collections.sort(number, comparator);
+
+        for (Student n:number)
+            System.out.println(n);
+    }
+}
